437e6a12b5c8253292ab17c875f8d23f97b3a90f
Hansen Idden <hansenidden@gmail.com>
2021-12-13 17:47:32 +0700

    Change some keybindings

--- src/GameState/Level1State.java
+++ src/GameState/Level1State.java
@@ -136,9 +136,9 @@ public class Level1State extends GameState {
     if (k == KeyEvent.VK_UP) player.setUp(true);
     if (k == KeyEvent.VK_DOWN) player.setDown(true);
     if (k == KeyEvent.VK_W) player.setJumping(true);
-    if (k == KeyEvent.VK_E) player.setGliding(true);
-    if (k == KeyEvent.VK_R) player.setScratching();
-    if (k == KeyEvent.VK_F) player.setFiring();
+    if (k == KeyEvent.VK_S) player.setGliding(true);
+    if (k == KeyEvent.VK_A) player.setScratching();
+    if (k == KeyEvent.VK_D) player.setFiring();
   }
 
   public void keyReleased(int k) {
@@ -147,6 +147,6 @@ public class Level1State extends GameState {
     if (k == KeyEvent.VK_UP) player.setUp(false);
     if (k == KeyEvent.VK_DOWN) player.setDown(false);
     if (k == KeyEvent.VK_W) player.setJumping(false);
-    if (k == KeyEvent.VK_E) player.setGliding(false);
+    if (k == KeyEvent.VK_S) player.setGliding(false);
   }
 }
